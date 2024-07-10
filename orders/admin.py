63f661e61d0c014833c90918d731bd6835837e74
JiwonHwang <cutty82432@gmail.com>
2024-07-10 12:37:43 +0900

    [Feat] 주소 검색 기능 추가.  Daum Address API 적용

--- orders/admin.py
+++ orders/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from .models import Item, Order
 
 # Register your models here.
-admin.site.register(Item)
-admin.site.register(Order)
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'item', 'size', 'quantity', 'total_price', 'status', 'preferred_delivery_date']
+    list_filter = ['status', 'preferred_delivery_date']
+    search_fields = ['user__username', 'item__name', 'name', 'contact', 'address', 'depositor_name']
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'category', 'price_per_small', 'price_per_big', 'price_per_unit']
+    list_filter = ['category']
+    search_fields = ['name']
--- orders/forms.py
+++ orders/forms.py
@@ -1,12 +1,14 @@
 from django import forms
 from .models import Order,Item
+from django.forms.widgets import DateInput
+from datetime import date, timedelta
 
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = [
-            'item', 'size', 'quantity', 'name', 'contact', 'address',
-            'preferred_delivery_date', 'depositor_name'
+            'item', 'size', 'quantity', 'name', 'contact', 'postcode',
+            'address', 'extra_address', 'detail_address', 'preferred_delivery_date', 'depositor_name'
         ]
         labels = {
             'item': '상품', 
@@ -14,9 +16,19 @@ class OrderForm(forms.ModelForm):
             'quantity' : '수량', 
             'name' : '이름', 
             'contact' : '연락처', 
-            'address' : '주소',
-            'preferred_delivery_date' : '배송희망일', 
+            'postcode': '우편번호',
+            'address': '주소',
+            'extra_address': '참고항목',
+            'detail_address': '상세주소',
+            'preferred_delivery_date': '배송희망일', 
             'depositor_name' : '입금자명',
+        }
+        widgets = {
+            'preferred_delivery_date': DateInput(attrs={
+                'type': 'date',
+                'min' : date.today().isoformat(),
+                'max' : (date.today() + timedelta(days=30)).isoformat()
+            }),
         }
     
     def __init__(self, *args, **kwargs):
