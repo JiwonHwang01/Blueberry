2d2e215b1c08372cec56974aeee326c0ba5180b8
JiwonHwang <cutty82432@gmail.com>
2024-07-11 12:25:02 +0900

    [admin] 어드민 검색 및 필터 기능 수정

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Review
 
-admin.site.register(Review)
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display= ['item', 'size', 'user', 'comment', 'image', 'created_at']
+    list_filter = ['created_at']
+    search_fields=['item__category', 'size', 'user__username']
