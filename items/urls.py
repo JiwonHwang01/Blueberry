d879dadcafc2118e0685adcbf09c0718df7ce5c6
JiwonHwang <cutty82432@gmail.com>
2024-07-12 16:28:23 +0900

    [refactor] Item앱에서 일부 기능 관리자 페이지로 이식

--- items/urls.py
+++ items/urls.py
@@ -4,6 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.item_list, name='item_list'),
     path('<int:pk>/', views.item_detail, name='item_detail'),
-    path('new/', views.item_create, name='item_create'),
-    path('<int:pk>/edit/', views.item_edit, name='item_edit'),
 ]
