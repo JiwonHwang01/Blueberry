bb540cc0a10f7c173ff62c49b03b4061d641c433
JiwonHwang <cutty82432@gmail.com>
2024-07-13 14:29:12 +0900

    [feat] Info 페이지 추가

--- orders/urls.py
+++ orders/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('create/', create_order, name='create_order'),
     path('success/', order_success, name='order_success'),
     path('list/', order_list, name='order_list'),
+    path('info/', views.info, name='info'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -26,3 +26,6 @@ def order_success(request):
 def order_list(request):
     orders = Order.objects.filter(user=request.user)
     return render(request, 'orders/order_list.html', {'orders': orders})
+
+def info(request):
+    return render(request, 'orders/info.html')
