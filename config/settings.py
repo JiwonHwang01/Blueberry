4c506d0ebb3579fc4c68a4480bd292a505c371c0
JiwonHwang <cutty82432@gmail.com>
2025-06-08 21:28:12 +0900

    yml 파일 수정

--- config/settings.py
+++ config/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o7b5(i64c@c7($!835=+(tdk_$3m^wjvhe*otmas25+xrpp(lf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.110.158.222', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -138,9 +138,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    BASE_DIR / "vanilla-frontend",
 ]
 
 # Default primary key field type
