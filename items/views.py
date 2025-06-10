4a6f1d7681fce4114d80b97c5133c30e58665a76
JiwonHwang <cutty82432@gmail.com>
2025-06-10 19:14:40 +0900

    deploy

--- items/views.py
+++ items/views.py
@@ -3,7 +3,6 @@ from .models import Item
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from .serializers import ItemSerializer
 
 def item_list(request):
     items = Item.objects.all()
