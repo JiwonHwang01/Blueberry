1786fc6927b27380624d698a264953a6cef7355c
JiwonHwang <cutty82432@gmail.com>
2025-06-13 09:35:10 +0900

    feat: items 이미지 추가

--- items/models.py
+++ items/models.py
@@ -12,6 +12,7 @@ class Item(models.Model):
     price_per_small = models.DecimalField(max_digits=10, decimal_places=0, null=True, blank=True)
     price_per_big = models.DecimalField(max_digits=10, decimal_places=0, null=True, blank=True)
     price_per_unit = models.DecimalField(max_digits=10, decimal_places=0, null=True, blank=True)
+    image = models.ImageField(upload_to='items/images/', null=True, blank=True)
 
     def __str__(self):
         return self.name
