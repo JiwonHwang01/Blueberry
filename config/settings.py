2adf6095ed00a656b9711a3ec00c15d6b44197f5
JiwonHwang <cutty82432@gmail.com>
2025-07-24 23:04:27 +0900

    deploy

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ BANK_ACCOUNT = {
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['3.110.158.222', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['52.79.236.66', 'localhost', '127.0.0.1']
 
 
 # Application definition
